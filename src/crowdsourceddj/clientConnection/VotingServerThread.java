82f70065e25741b5ac98ddb9b96dcf4f3d889703
docksmur <murdock.walsh@gmail.com>
2016-01-21 13:28:35 -0500

    Repair client connection
    hopefully

--- src/crowdsourceddj/clientConnection/VotingServerThread.java
+++ src/crowdsourceddj/clientConnection/VotingServerThread.java
@@ -5,10 +5,10 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
 
-import crowdsourceddj.genreradio.DynamicPlaylister;
 import crowdsourceddj.utils.GenreUtils;
 
 public class VotingServerThread extends Thread {
+	
 	private Socket clientSocket;
 	
 	public VotingServerThread(Socket client){
@@ -22,19 +22,25 @@ public class VotingServerThread extends Thread {
 			Scanner sc = new Scanner(clientSocket.getInputStream());
 			String message;
 			boolean quit = false;
-			while(!quit && !clientSocket.isClosed()){
-				if(sc.hasNextLine()){
-					message = sc.next();
-					System.out.println("Got message from device: "+message);
-					if(message.contains(";") && ProxDetector.checkAddress(message.split(";")[1])){
-						GenreUtils.addGenre(message.split(";")[0]);
-					}
-					quit = true;
+			if(sc.hasNextLine()){
+				message = sc.next();
+				System.out.println("Got message from device: "+message);
+				if(ProxDetector.getHostDevice()==null){
+					ProxDetector.setHostDevice(clientSocket);
+				}else{
+					out.println("done: done");
+					quit=true;
+				}
+				if(message.contains(";") && ProxDetector.checkAddress(message.split(";")[1])){
+					GenreUtils.addGenre(message.split(";")[0]);
 				}
 			}
-			sc.close();
-			out.close();
-			clientSocket.close();
+			if(quit){
+				sc.close();
+				out.close();
+				clientSocket.close();
+			}
+			return;
 		}catch (IOException e){
 			e.printStackTrace();
 		}
